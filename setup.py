eb57535b46c4730daff624d8a8ff10c6de2d6ef2
wael-sudo2 <wael.hkiri.dev@gmail.com>
2023-05-15 16:25:00 +0100

    intial_commit_fix_setup_file

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name='facebook-page-info-scraper',
     version='1.0.0',
+    author='Wael hkiri',
+    author_email='wael.hkiri.dev@gmail.com',
+    description='a python package capable of crawling facebook page information',
     packages=find_packages(),
     install_requires=[
         'geopy==2.2.0',
