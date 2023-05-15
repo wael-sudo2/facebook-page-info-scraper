a4ddeaa59e4e21d43949f75b6a9abb9f1fc1def8
sp0T <106942086+wael-sudo2@users.noreply.github.com>
2023-05-15 20:55:09 +0100

    Revert "change author name"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='facebook-page-info-scraper',
     version='1.0.0',
-    author='sp0T',
+    author='Wael hkiri',
     author_email='wael.hkiri.dev@gmail.com',
     description='a python package capable of crawling facebook page information',
     packages=find_packages(),
