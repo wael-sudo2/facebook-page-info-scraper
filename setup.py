8e73a7441ca30cc2f56cd7079fabdc33082782e0
wael-sudo2 <wael.hkiri.dev@gmail.com>
2023-05-18 13:27:05 +0100

    [FIX] changes 2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name='facebook_page_info_scraper',
-    version='1.0.6',
+    version='1.0.7',
     author='Wael hkiri',
     author_email='wael.hkiri.dev@gmail.com',
     description='A Python package capable of crawling Facebook page information',
